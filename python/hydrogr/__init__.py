69a33391a8b0981d26f4bbca12f7816a02552ada
Simon Delmas <simon.delmas@suez.com>
2023-02-27 21:50:23 +0100

    Import models in package init file for easier access

--- python/hydrogr/__init__.py
+++ python/hydrogr/__init__.py
@@ -1,2 +1,9 @@
 __version__ = "0.1.0"
 
+from hydrogr.input_data import InputDataHandler
+from hydrogr.gr1a import ModelGr1a
+from hydrogr.gr2m import ModelGr2m
+from hydrogr.gr4j import ModelGr4j
+from hydrogr.gr5j import ModelGr5j
+from hydrogr.gr6j import ModelGr6j
+from hydrogr.gr4h import ModelGr4h
